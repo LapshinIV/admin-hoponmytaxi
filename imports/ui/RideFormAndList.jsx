fe9b03a4cd1e1306ecf25995354d597246c40bd1
Ivan <ivan.lapshin@39parrot.net>
2016-08-01 23:30:59 +0400

    add lost files

---
+++ imports/ui/RideFormAndList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
+
+import RideForm from './RideForm.jsx';
+import RideList from './RideList.jsx';
+
+import { Airports } from '../../both/collections.js';
+
+class App extends Component {
+  render() {
+    // console.log('App.render')
+    return (      
+        <table>
+          <tbody>
+            <tr>
+              <td style={{border: "5px solid yellow"}}>
+                {/* {{> ride_form model=rideModel callbacks=rideCRUDCallbacks isEditingMode=isRideFormEditingMode }}*/}
+                <RideForm
+                  ride={this.props.rideFormModel}
+                  mode={this.props.rideInEdit ? 'edit' : 'new'}
+                  airports={this.props.airports} />
+              </td>
+              <td style={{width: "30px"}}></td>
+              <td style={{border: "5px solid blue"}}>
+                {/* {{> ride_list callbacks=rideCRUDCallbacks }}*/}
+                <RideList />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    );
+  }
+}
+
+export default createContainer(() => {
+  return {
+    rideFormModel: Session.get('rideFormModel') || {},
+    rideInEdit: Session.get('rideInEdit') && Session.get('rideFormModel') || null,
+    airports: Airports.find().fetch(),
+  }
+}, App);
